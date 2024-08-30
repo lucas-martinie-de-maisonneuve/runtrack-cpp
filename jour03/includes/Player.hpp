5096e47930160f3e182062716d6c307f1aff62d1
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-08-30 14:43:53 +0200

    <terminé>  - Choix des actions pour l'utilisateur

--- jour03/includes/Player.hpp
+++ jour03/includes/Player.hpp
@@ -10,22 +10,20 @@
 class Player : public Character
 {
 private:
-    Weapon *weapons[3]; // Tableau pour les armes disponibles
+    Weapon *weapons[3];
     int currentWeaponIndex;
 
 public:
     Player(const std::string &name, int hp, double x, double y)
         : Character(name, hp, x, y), currentWeaponIndex(0)
     {
-        // Initialisation des armes disponibles
-        weapons[0] = new Bow();   // Assurez-vous que Bow est correctement défini
-        weapons[1] = new Spear(); // Assurez-vous que Spear est correctement défini
-        weapons[2] = new Sword(); // Assurez-vous que Sword est correctement défini
+        weapons[0] = new Bow();
+        weapons[1] = new Spear();
+        weapons[2] = new Sword();
     }
 
     ~Player()
     {
-        // Nettoyage des armes
         for (Weapon *weapon : weapons)
         {
             delete weapon;
@@ -48,49 +46,127 @@ public:
 
     void update(Character &target) override
     {
-        char move;
-        cout << "Chose z,q,s,d to move : ";
-        cin >> move;
+        bool hasMoved = false;
+        bool hasAttacked = false;
+        bool turnOver = false;
 
-        switch (move)
+        while (!turnOver)
         {
-        case 'z':
-            setY(getY() - 1);
-            break;
-        case 'q':
-            setX(getX() - 1);
-            break;
-        case 's':
-            setY(getY() + 1);
-            break;
-        case 'd':
-            setX(getX() + 1);
-            break;
-        }
+            char action;
+            std::cout << "\n"
+                      << "                   Choose an action\n"
+                      << "       +-----------------------------------------+\n"
+                      << "       |    [1] Switch Weapon                    |\n"
+                      << "       |    [2] Move                             |\n"
+                      << "       |    [3] Attack                           |\n"
+                      << "       |    [4] End Turn                         |\n"
+                      << "       +-----------------------------------------+\n\t";
 
-        std::cout << getName() << " moved to (" << getX() << ", " << getY() << ")" << std::endl;
+            std::cin >> action;
 
-        // Tenter une attaque après le déplacement
-        attack(target);
+            switch (action)
+            {
+            case '1':
+                std::cout << "\n"
+                          << "                   Choose your weapon\n"
+                          << "       +-----------------------------------------+\n"
+                          << "       |    [1] Bow                              |\n"
+                          << "       |    [2] Spear                            |\n"
+                          << "       |    [3] Sword                            |\n"
+                          << "       +-----------------------------------------+\n\t";
+                int weaponChoice;
+                std::cin >> weaponChoice;
 
-        // Changer d'arme pour le prochain tour
-        currentWeaponIndex = (currentWeaponIndex + 1) % 3;
-        std::cout << getName() << " switched to ";
-        if (currentWeaponIndex == 0)
-        {
-            std::cout << "Bow";
+                if (weaponChoice < 1 || weaponChoice > 3)
+                {
+                    std::cout << "!!! Invalid weapon choice !!!" << std::endl;
+                }
+                else
+                {
+                    std::cout << getName() << " switched to ";
+                    switch (weaponChoice)
+                    {
+                    case 1:
+                        std::cout << "Bow" << std::endl;
+                        currentWeaponIndex = 0;
+                        break;
+                    case 2:
+                        std::cout << "Spear" << std::endl;
+                        currentWeaponIndex = 1;
+                        break;
+                    case 3:
+                        std::cout << "Sword" << std::endl;
+                        currentWeaponIndex = 2;
+                        break;
+                    }
+                }
+                break;
+
+            case '2':
+                if (hasMoved)
+                {
+                    std::cout << "!!! You can only move once per turn !!!" << std::endl;
+                }
+                else
+                {
+                    char move;
+                    std::cout << "\n"
+                              << "                 Use Z, Q, S, D to move\n"
+                              << "       +-----------------------------------------+\n"
+                              << "       |                   Up                    |\n"
+                              << "       |                  [Z]                    |\n"
+                              << "       |         Left [Q] [S] [D] Right          |\n"
+                              << "       |                 Down                    |\n"
+                              << "       +-----------------------------------------+\n\t";
+                    std::cin >> move;
+
+                    switch (move)
+                    {
+                    case 'z':
+                        setY(getY() - 1);
+                        break;
+                    case 'q':
+                        setX(getX() - 1);
+                        break;
+                    case 's':
+                        setY(getY() + 1);
+                        break;
+                    case 'd':
+                        setX(getX() + 1);
+                        break;
+                    default:
+                        std::cout << "!!! Invalid move direction !!!" << std::endl;
+                        continue;
+                    }
+
+                    std::cout << getName() << " moved to (" << getX() << ", " << getY() << ")" << std::endl;
+                    hasMoved = true;
+                }
+                break;
+
+            case '3':
+                if (hasAttacked)
+                {
+                    std::cout << "!!! You can only attack once per turn !!!" << std::endl;
+                }
+                else
+                {
+                    attack(target);
+                    hasAttacked = true;
+                }
+                break;
+
+            case '4':
+                std::cout << getName() << " ended their turn." << std::endl;
+                turnOver = true;
+                break;
+
+            default:
+                std::cout << "!!! Invalid action! Please choose a valid option !!!" << std::endl;
+                break;
+            }
         }
-        else if (currentWeaponIndex == 1)
-        {
-            std::cout << "Spear";
-        }
-        else
-        {
-            std::cout << "Sword";
-        }
-        std::cout << std::endl;
     }
-
     void draw() const override
     {
         std::cout << "Drawing Player " << getName() << " at ";
